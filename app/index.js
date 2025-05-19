c6f4f021420d424f1ba168bb7869787f4f8edbf6
deepdeb <debnath.pradeep619@gmail.com>
2025-05-19 18:03:11 +0530

    added styles to views & texts and added Image

--- app/index.js
+++ app/index.js
@@ -1,14 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import Logo from '../assets/img/read.jpeg'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image style={styles.logo} source={Logo} />
+      <Text style={styles.title}>The Number 1</Text>
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+  logo: {
+    height: 50,
+    width: 50,
+    marginVertical: 20
+  }
+})
